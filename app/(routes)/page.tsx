7ead387d806f523296dc7d1bd89e4659b54e0836
hamrajk <hamraj_khangura@hotmail.com>
2023-11-08 21:37:09 -0500

    added product and product list pages, populated with image previews and summaries on product cards. all fetched from prisma db

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,25 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("0c44292e-81c5-4566-a837-a2f03c2f89a0");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
