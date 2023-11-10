ebff9c7866d7eefb3b73d9589988db8dbf9de87d
hamrajk <hamraj_khangura@hotmail.com>
2023-11-09 22:59:06 -0500

    set up preview modal for products

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -7,6 +7,8 @@ import Container from "@/components/ui/container";
 import NoResults from "@/components/ui/no-results";
 
 import Filter from "./components/filter";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -30,8 +32,6 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     sizeId: searchParams.sizeId,
   });
 
-  console.log("products", products);
-
   const sizes = await getSizes();
   const colors = await getColors();
   const category = await getCategory(params.categoryId);
@@ -42,13 +42,18 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add Mobile Filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
             <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
